eb8a791d4776e8471b65bb95c698120367060d63
Chingiz2727 <k.chyngiz@gmail.com>
2019-07-15 14:55:29 +0600

    Initial Commit

---
+++ EuroPharmaIOSapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EuroPharmaIOSapp
+//
+//  Created by Shyngys Kuandyk on 7/15/19.
+//  Copyright © 2019 Shyngys Kuandyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
